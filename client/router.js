97fd116abcf9bd00e5e5030d6a6e5cae12d064f9
Nick <nick.bucheleres@gmail.com>
2015-05-03 15:55:08 -0700

    non logged in people cant see admin page

--- client/router.js
+++ client/router.js
@@ -3,3 +3,20 @@
 Router.configure({
 	layoutTemplate: 'layoutTemplate'
 });
+
+
+var adminLogin;
+
+adminLogin = {
+	loginRequired: function(){
+		if(!Meteor.userId()){
+			this.render('loginTemplate');
+		} else {
+			this.next();
+		}
+	}
+};
+
+Router.onBeforeAction(adminLogin.loginRequired, {
+	only: 'admin'
+});
